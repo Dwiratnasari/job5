1938c739d7a7dafc7b3f0b0096d84d3354925d66
Dwi Ratnasari <dwiratnasari391@gmail.com>
2017-05-29 14:15:47 +0700

    Add files via upload

---
+++ function_parameter.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Function Dengan Nilai parameter</title>
+</head>
+<body>
+<?php
+echo"<b>";
+function panggil($parameter)
+{
+echo"Saya Bilang : $parameter";
+echo"<br>";
+}
+panggil("Belajar PHP rupanya <u>mudah</u>");
+panggil("Asyik...");
+panggil("Dan Sangat Menyenangkan");
+panggil("Nilai saya php adalah 98");
+echo"</b>";
+?>
+</body>
+</html>
